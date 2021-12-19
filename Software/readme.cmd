1fd924e7a881aa5619a48ea9aea18693d8fe1944
abu-mejia <abraham.mejia.aguilar@gmail.com>
2021-12-19 20:54:56 +0100

    Update readme.cmd

--- Software/readme.cmd
+++ Software/readme.cmd
@@ -1,1 +1,10 @@
 This folder contains some of the scripts that are used for the processing and analysis of data provided by the instruments
+
+- PCA_KnowledgeAmplifier
+Simplified algorithm to calculate Principal Component Analysis (PCA) to differentiate between infected and non infected trees using the Spectroradiometer HR1024i
+
+- HR1024_V2
+Extracts the subset of spectral signatures and creates three different CSV files for ingesting them in PCA. Transforms RAW data from the Spectroradiometer HR1024i to CSV
+
+- GCP_Leica_XLSX_CSV
+Process the Data provided by the Leica instrument. It is necessary to process this data in order to create high accurate points for Precision Mapping Agriculture Mosaics
